4870eee7b2eeed71b168693427648b0890ee71f0
AnnetaDe <anneta.liss@gmail.com>
2025-06-05 16:14:57 +0200

    Revert "Merge pull request #6 from AnnetaDe/2ysru7-codex/add-jest-and-create-httperror-tests"
    This reverts commit 139c498cb511b9e7b23db701f72dc4a1a8089118, reversing
    changes made to 28fc6010aab8ef71c81422ab381ccf69ea4a4e25.

--- tests/helpers/HttpError.test.ts
+++
@@ -1,9 +0,0 @@
-import HttpError from '../../src/helpers/HttpError';
-
-describe('HttpError', () => {
-  it('stores statusCode and sanitized message', () => {
-    const err = new HttpError(400, 'Bad');
-    expect(err.statusCode).toBe(400);
-    expect(err.message).toBe('Bad');
-  });
-});
